148de72d1adc421a15431560da27aa58975c19a1
BrandonDeB <brandonrdebaroncelli@gmail.com>
2025-08-22 22:06:52 -0500

    Removed SQL implementation  - Added connection to mongo

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,8 +1,8 @@
 use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
 use serde::{Serialize, Deserialize};
-use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
 use time::Date;
+
+mod mongo;
 
 #[derive(Deserialize)]
 struct WorkoutRequest {
@@ -30,89 +30,15 @@ struct Exercise {
     date: Option<Date>,
 }
 
-#[get("/hello/{name}")]
-pub async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
-}
-
-#[get("/exercises")]
-pub async fn get_exercises(
-    pool: web::Data<PgPool>,
-    query: web::Query<ExerciseRequest>,
-) -> impl Responder {
-    let owner_id = query.owner_id;
-    let date = query.date;
-    let result = sqlx::query_as!(
-        Exercise,
-        r#"
-        SELECT 
-            e.id, 
-            e.name, 
-            DATE(w.created_at)
-        FROM template_exercises te
-        JOIN workouts w ON w.template_id = te.template_id
-        JOIN exercises e ON e.id = te.exercise_id
-        WHERE w.owner_id = $1 
-          AND DATE(w.created_at) = $2
-        "#,
-        owner_id,
-        date
-    )
-    .fetch_all(pool.get_ref())
-    .await;
-
-    match result {
-        Ok(exercises) => HttpResponse::Ok().json(exercises),
-        Err(e) => {
-            eprintln!("Database error: {}", e);
-            HttpResponse::InternalServerError().body("Failed to fetch workouts")
-        }
-    }
-}
-#[get("/workouts")]
-pub async fn get_workouts(
-    pool: web::Data<PgPool>,
-    query: web::Query<WorkoutRequest>,
-) -> impl Responder {
-    let owner_id = query.owner_id;
-
-    let result = sqlx::query_as!(
-        Workout,
-        r#"
-        SELECT id, template_id, DATE(created_at), owner_id
-        FROM workouts
-        WHERE owner_id = $1
-        ORDER BY created_at DESC
-        LIMIT 5
-        "#,
-        owner_id
-    )
-    .fetch_all(pool.get_ref())
-    .await;
-
-    match result {
-        Ok(workouts) => HttpResponse::Ok().json(workouts),
-        Err(e) => {
-            eprintln!("Database error: {}", e);
-            HttpResponse::InternalServerError().body("Failed to fetch workouts")
-        }
-    }
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
-        .await
-        .expect("Failed to connect to the database");
 
+    let client = mongo::create_client()
+        .await
+        .expect("Create client failed");
+    
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .service(greet)
-            .service(get_workouts)
-            .service(get_exercises)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- web-server/src/mongo.rs
+++ web-server/src/mongo.rs
@@ -1,28 +1,37 @@
+use std::env;
+use dotenv::dotenv;
 use mongodb::{bson::doc, options::{ClientOptions, ServerApi, ServerApiVersion}, Client};
 
-#[tokio::main]
-async fn main() -> mongodb::error::Result<()> {
+pub async fn create_client() -> mongodb::error::Result<Client> {
+    dotenv().ok();
+
+
     let user = env::var("MONGO_USER").expect("Mongo user not set");
     let password = env::var("MONGO_PASSWORD").expect("Mongo password not set");
     let uri = format!("mongodb+srv://{user}:{password}@songguessr.quhlriw.mongodb.net/?retryWrites=true&w=majority&appName=SongGuessr");
-  let mut client_options =
-    ClientOptions::parse(uri)
-      $.await?;
+
+    let mut client_options =
+        ClientOptions::parse(uri)
+        .await
+        .expect("The client options failed");
 
   // Set the server_api field of the client_options object to set the version of the Stable API on the client
-  let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
-  client_options.server_api = Some(server_api);
+    let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
+    client_options.server_api = Some(server_api);
 
   // Get a handle to the cluster
-  let client = Client::with_options(client_options)?;
+    let client = Client::with_options(client_options)?;
 
   // Ping the server to see if you can connect to the cluster
-  client
-    .database("admin")
-    .run_command(doc! {"ping": 1}, None)
-    .await?;
+    client
+        .database("admin")
+        .run_command(doc! {"ping": 1})
+        .await
+        .expect("The client couldn't run the command");
 
-  println!("Pinged your deployment. You successfully connected to MongoDB!");
+    println!("Pinged your deployment. You successfully connected to MongoDB!");
 
-  Ok(())
+    Ok(client)
+
 }
+
