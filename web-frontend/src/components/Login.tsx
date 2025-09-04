1dd04a1f18b4f3e1a0f3b02ba231935d1fb8eea6
BrandonDeB <brandonrdebaroncelli@gmail.com>
2025-09-04 13:54:03 -0500

    Added firebase authentication

--- web-frontend/src/components/Login.tsx
+++ web-frontend/src/components/Login.tsx
@@ -1,8 +1,29 @@
-function Login() {
-    return (
-        <>
+import {Button} from "@mui/material";
+import {useState} from "react";
+import {GithubAuthProvider, signInWithPopup} from "firebase/auth";
+import {auth} from "../firebase.tsx";
 
-        </>
+function Login() {
+
+    const [isPending, setIsPending] = useState(false);
+    const provider = new GithubAuthProvider();
+
+    const login = async () => {
+        setIsPending(true);
+
+        const res = await signInWithPopup(auth, provider);
+        if (!res) {
+            console.log("Error logging in");
+            setIsPending(false);
+        } else {
+            const user = res.user;
+            console.log(user);
+            setIsPending(false)
+        }
+    };
+
+    return (
+        <Button variant="outlined" onClick={login}>{isPending ? "Pending" : "Login with Github"}</Button>
     );
 }
 
--- web-frontend/src/main.tsx
+++ web-frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import Form from "./components/Form.tsx";
+import App from "./App.tsx";
+import {StrictMode} from "react";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Form />
-  </StrictMode>,
+    <StrictMode>
+        <App/>
+    </StrictMode>
 )
